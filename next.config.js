4d93ccb9dc3b9896b4fe692e72b5171d650921e0
unknown <trongdatga@gmail.com>
2025-11-05 16:55:19 +0700

    [fix error deploy] stop Edge Runtime

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
-  }
-}
+  images: {
+    domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+  },
+  experimental: {
+    runtime: "nodejs",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
